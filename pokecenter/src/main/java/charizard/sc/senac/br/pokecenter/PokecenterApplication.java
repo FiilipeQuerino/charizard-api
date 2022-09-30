4afc26bcbf1000bc17a02ff74643dd57d3c4ec43
FiilipeQuerino <flpqrn@gmail.com>
2022-09-30 00:07:15 -0300

    Criado projeto e conexão com o banco.

---
+++ pokecenter/src/main/java/charizard/sc/senac/br/pokecenter/PokecenterApplication.java
@@ -0,0 +1,13 @@
+package charizard.sc.senac.br.pokecenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokecenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokecenterApplication.class, args);
+	}
+
+}
---
+++ pokecenter/src/test/java/charizard/sc/senac/br/pokecenter/PokecenterApplicationTests.java
@@ -0,0 +1,13 @@
+package charizard.sc.senac.br.pokecenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokecenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
